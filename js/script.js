9d951a45982a08dd1e7f23c528daa34aa75d649c
Zofia <z.przybyl11@gmail.com>
2022-09-23 16:34:42 +0000

    Add boxes in html

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-$('button').on("mouseenter", function() {
-	$(this).removeClass('makeRed').addClass('makeBlue');
-});
-
-$('button').on("mouseleave", function() {
-	$(this).removeClass('makeBlue').addClass('makeRed');
+$('.box').on("click", function(){
+    var classNames = $(this).attr("class").split(" ");
+    $("." + classNames[1]).css('background-color', 'red');
 });
 
